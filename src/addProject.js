5bfd2d82691c95f56a1b7d660c1d1cdb3b51bf1c
Nikola Nekudova <nikolka.nekudova@seznam.cz>
2022-10-25 21:05:40 +0200

    feat(): adding projects is prettier n2

--- src/addProject.js
+++ src/addProject.js
@@ -1,9 +1,6 @@
-const addProjectButton = document.getElementById("btn-add-project");
-const projectsDiv = document.getElementById("projects-wrapper");
-const projectsListDiv = document.getElementById("projects-list");
-
 export let myProjects = [];
 
+const addProjectButton = document.getElementById("btn-add-project");
 addProjectButton.addEventListener("click", showInputAddProject);
 
 generateInputAddProject();
@@ -16,6 +13,8 @@ function showInputAddProject() {
 }
 
 export function generateInputAddProject() {
+    const projectsDiv = document.getElementById("projects-wrapper");
+
     const addProjectDiv = document.createElement("div");
     addProjectDiv.setAttribute("id", "add-project");
     addProjectDiv.style.display = "none";
@@ -75,6 +74,8 @@ function cancelAddingProject() {
 }
 
 function showProjectsInMenu() {
+    const projectsListDiv = document.getElementById("projects-list");
+
     for (let i = 0; i < myProjects.length; i++) {
         const divForProject = document.createElement("div");
         divForProject.setAttribute("class", "button-icon-wrapper")
@@ -91,72 +92,9 @@ function showProjectsInMenu() {
 }
 
 function deleteProjectsInMenu() {
+    const projectsListDiv = document.getElementById("projects-list");
+
     while (projectsListDiv.firstChild) {
         projectsListDiv.removeChild(projectsListDiv.firstChild);
     }
 }
-
-
-/* export function addProject() {
-
-    addProjectButton.style.visibility = "hidden";
-    addProjectButton.style.display = "none";
-
-    const addProjectDiv = document.createElement("div");
-    addProjectDiv.setAttribute("class", "add-project");
-    projectsDiv.appendChild(addProjectDiv);
-
-    const addProjectInput = document.createElement("input");
-    addProjectInput.setAttribute("type", "text");
-    addProjectInput.setAttribute("id", "project-input");
-    addProjectDiv.appendChild(addProjectInput);
-
-    const buttonsDiv = document.createElement("div");
-    buttonsDiv.setAttribute("id", "btns-add-project-dom");
-    projectsDiv.appendChild(buttonsDiv);
-
-    const buttonAddProject = document.createElement("button");
-    buttonAddProject.innerHTML = "Add";
-    buttonAddProject.setAttribute("id", "btn-add-project-dom");
-    buttonsDiv.appendChild(buttonAddProject);
-
-    const buttonCancelAddProject = document.createElement("button");
-    buttonCancelAddProject.innerHTML = "Cancel";
-    buttonCancelAddProject.setAttribute("id", "btn-cancel-add-project-dom");
-    buttonsDiv.appendChild(buttonCancelAddProject);
-
-    // jak funkci dát ven, aby se dala použít jinde? proměnné do parametru mi dát nejdou  
-
-    buttonAddProject.addEventListener("click", function addNewProject() {
-        myProjects.push(addProjectInput.value);
-        addProjectInput.value = "";
-
-        deleteProjectInMenu();
-        showProjectsInMenu();
-        buttonCancelAddProject.click();
-    })
-
-    buttonCancelAddProject.addEventListener("click", function cancelAddingProject() {
-        addProjectButton.style.visibility = "visible";
-        addProjectButton.style.display = "";
-
-        addProjectDiv.style.visibility = "hidden";
-        addProjectDiv.style.display = "none";
-
-        buttonsDiv.style.visibility = "hidden";
-        buttonsDiv.style.display = "none";
-    })
-
-    // klávesa enter > nový projekt
-    addProjectInput.addEventListener("keyup", function (event) {
-        if (event.code === 'Enter') {
-            myProjects.push(addProjectInput.value);
-            addProjectInput.value = "";
-
-            deleteProjectInMenu();
-            showProjectsInMenu();
-            buttonCancelAddProject.click();
-        }
-    });
-
-} */
