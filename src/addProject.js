49193da4fb97027864964e2c238319c2e95cf09a
Nikola Nekudova <nikolka.nekudova@seznam.cz>
2022-10-25 21:02:05 +0200

    feat(): projects functions are prettier

--- src/addProject.js
+++ src/addProject.js
@@ -3,12 +3,101 @@ const projectsDiv = document.getElementById("projects-wrapper");
 const projectsListDiv = document.getElementById("projects-list");
 
 export let myProjects = [];
-//local storage?
+
+addProjectButton.addEventListener("click", showInputAddProject);
+
+generateInputAddProject();
+
+function showInputAddProject() {
+    const addProjectDiv = document.getElementById("add-project");
+
+    addProjectDiv.style.display = "block";
+    addProjectButton.style.display = "none";
+}
+
+export function generateInputAddProject() {
+    const addProjectDiv = document.createElement("div");
+    addProjectDiv.setAttribute("id", "add-project");
+    addProjectDiv.style.display = "none";
+    projectsDiv.appendChild(addProjectDiv);
+
+    const addProjectInput = document.createElement("input");
+    addProjectInput.setAttribute("type", "text");
+    addProjectInput.setAttribute("id", "project-input");
+    addProjectDiv.appendChild(addProjectInput);
+
+    const buttonsDiv = document.createElement("div");
+    buttonsDiv.setAttribute("id", "btns-add-project-dom");
+    addProjectDiv.appendChild(buttonsDiv);
+
+    const buttonAddProject = document.createElement("button");
+    buttonAddProject.innerHTML = "Add";
+    buttonAddProject.setAttribute("id", "btn-add-project-dom");
+    buttonsDiv.appendChild(buttonAddProject);
+
+    buttonAddProject.addEventListener("click", addNewProject);
+
+    const buttonCancelAddProject = document.createElement("button");
+    buttonCancelAddProject.innerHTML = "Cancel";
+    buttonCancelAddProject.setAttribute("id", "btn-cancel-add-project-dom");
+    buttonsDiv.appendChild(buttonCancelAddProject);
+
+    buttonCancelAddProject.addEventListener("click", cancelAddingProject);
+
+    // klávesa enter > nový projekt
+    addProjectInput.addEventListener("keyup", function (event) {
+        if (event.code === 'Enter') {
+            addNewProject();
+        }
+    });
+}
+
+function addNewProject() {
+    const addProjectInput = document.getElementById("project-input");
+    const addNewProjectButton = document.getElementById("btn-add-project");
+    const addNewProjectInputWrapper = document.getElementById("add-project");
+
+    myProjects.push(addProjectInput.value);
+    addProjectInput.value = "";
+
+    deleteProjectsInMenu();
+    showProjectsInMenu();
+
+    addNewProjectButton.style.display = "flex";
+    addNewProjectInputWrapper.style.display = "none";
+}
+
+function cancelAddingProject() {
+    const addProjectDiv = document.getElementById("add-project");
+
+    addProjectButton.style.display = "flex";
+    addProjectDiv.style.display = "none";
+}
+
+function showProjectsInMenu() {
+    for (let i = 0; i < myProjects.length; i++) {
+        const divForProject = document.createElement("div");
+        divForProject.setAttribute("class", "button-icon-wrapper")
+        projectsListDiv.appendChild(divForProject);
+
+        const iconProject = document.createElement("i");
+        iconProject.setAttribute("class", "fa-solid fa-list-check");
+        divForProject.appendChild(iconProject);
+
+        const nameOfProject = document.createElement("div");
+        nameOfProject.innerHTML = myProjects[i];
+        divForProject.appendChild(nameOfProject);
+    }
+}
+
+function deleteProjectsInMenu() {
+    while (projectsListDiv.firstChild) {
+        projectsListDiv.removeChild(projectsListDiv.firstChild);
+    }
+}
 
 
-addProjectButton.addEventListener("click", addProject);
-
-export function addProject() {
+/* export function addProject() {
 
     addProjectButton.style.visibility = "hidden";
     addProjectButton.style.display = "none";
@@ -19,7 +108,7 @@ export function addProject() {
 
     const addProjectInput = document.createElement("input");
     addProjectInput.setAttribute("type", "text");
-    addProjectInput.setAttribute("class", "project-input");
+    addProjectInput.setAttribute("id", "project-input");
     addProjectDiv.appendChild(addProjectInput);
 
     const buttonsDiv = document.createElement("div");
@@ -44,6 +133,7 @@ export function addProject() {
 
         deleteProjectInMenu();
         showProjectsInMenu();
+        buttonCancelAddProject.click();
     })
 
     buttonCancelAddProject.addEventListener("click", function cancelAddingProject() {
@@ -57,8 +147,6 @@ export function addProject() {
         buttonsDiv.style.display = "none";
     })
 
-
-
     // klávesa enter > nový projekt
     addProjectInput.addEventListener("keyup", function (event) {
         if (event.code === 'Enter') {
@@ -67,30 +155,8 @@ export function addProject() {
 
             deleteProjectInMenu();
             showProjectsInMenu();
+            buttonCancelAddProject.click();
         }
     });
 
-}
-
-function showProjectsInMenu() {
-
-    for (let i = 0; i < myProjects.length; i++) {
-        const divForProject = document.createElement("div");
-        divForProject.setAttribute("class", "button-icon-wrapper")
-        projectsListDiv.appendChild(divForProject);
-
-        const iconProject = document.createElement("i");
-        iconProject.setAttribute("class", "fa-solid fa-list-check");
-        divForProject.appendChild(iconProject);
-
-        const nameOfProject = document.createElement("div");
-        nameOfProject.innerHTML = myProjects[i];
-        divForProject.appendChild(nameOfProject);
-    }
-}
-
-function deleteProjectInMenu() {
-    while (projectsListDiv.firstChild) {
-        projectsListDiv.removeChild(projectsListDiv.firstChild);
-    }
-}
+} */
--- src/inbox.js
+++ src/inbox.js
@@ -2,17 +2,31 @@ const header = document.getElementById("content-header");
 const pageContent = document.getElementById("content");
 const buttonInbox = document.getElementById("inbox-div");
 
+export let myToDoList = [{ id: 0, project: "inbox", title: "vzorový task", description: "vzorový description", dueDate: undefined, priority: undefined }];
+
 buttonInbox.addEventListener("click", showInbox);
 
+function Task(id, project, title, description, dueDate, priority) {
+    this.id = id;
+    this.project = project;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+}
+
+showTasks();
 showInbox();
 
 export function showInbox() {
+    // "zakliknutí" v menu
     buttonInbox.setAttribute("class", "button-icon-wrapper-checked");
 
+    // vygeneruje inbox stránku
     header.innerHTML = "Inbox";
 
     const buttonAddTask = document.createElement("div");
-    buttonAddTask.setAttribute("class", "btn-add-task");
+    buttonAddTask.setAttribute("id", "btn-add-task");
     pageContent.appendChild(buttonAddTask);
 
     const iconButtonAddTask = document.createElement("i");
@@ -23,5 +37,138 @@ export function showInbox() {
     textButtonAddTask.innerHTML = "Add Task";
     buttonAddTask.appendChild(textButtonAddTask);
 
-    
+
+    buttonAddTask.addEventListener("click", function addNewTask() {
+
+        buttonAddTask.style.visibility = "hidden";
+        buttonAddTask.style.display = "none";
+
+        // vytvoří div, input, div, 2x button pro přidání nového tasku
+        const addTaskDiv = document.createElement("div");
+        addTaskDiv.setAttribute("class", "add-task");
+        pageContent.appendChild(addTaskDiv);
+
+        const addTaskInput = document.createElement("input");
+        addTaskInput.setAttribute("type", "text");
+        addTaskInput.setAttribute("class", "task-input");
+        addTaskDiv.appendChild(addTaskInput);
+
+        const buttonsTaskDiv = document.createElement("div");
+        buttonsTaskDiv.setAttribute("id", "btns-add-task-dom");
+        addTaskDiv.appendChild(buttonsTaskDiv);
+
+        const buttonAddTaskDiv = document.createElement("button");
+        buttonAddTaskDiv.innerHTML = "Add";
+        buttonAddTaskDiv.setAttribute("id", "btn-add-task-dom");
+        buttonsTaskDiv.appendChild(buttonAddTaskDiv);
+
+        const buttonCancelAddTask = document.createElement("button");
+        buttonCancelAddTask.innerHTML = "Cancel";
+        buttonCancelAddTask.setAttribute("id", "btn-cancel-add-task-dom");
+        buttonsTaskDiv.appendChild(buttonCancelAddTask);
+
+
+        buttonAddTaskDiv.addEventListener("click", function addNewTask() {
+            let titleName = addTaskInput.value;
+            let index = Math.floor(Math.random() * 1000);
+
+            // id, project, title, description, dueDate, priority
+            let task = new Task(index, "inbox", titleName);
+            myToDoList.push(task);
+
+            addTaskInput.value = "";
+
+            deleteTasksInDiv();
+            showTasks();
+
+            // zmáčne za mě klávesu "cancel"
+            buttonCancelAddTask.click();
+        })
+
+        // všechno delete?
+        buttonCancelAddTask.addEventListener("click", function cancelAddingTask() {
+            buttonAddTask.style.visibility = "visible";
+            buttonAddTask.style.display = "";
+
+            addTaskDiv.style.visibility = "hidden";
+            addTaskDiv.style.display = "none";
+
+            buttonsTaskDiv.style.visibility = "hidden";
+            buttonsTaskDiv.style.display = "none";
+        })
+
+        // duplicita??
+        addTaskInput.addEventListener("keyup", function (event) {
+            if (event.code === 'Enter') {
+                let titleName = addTaskInput.value;
+                let index = Math.floor(Math.random() * 1000);
+
+                // id, project, title, description, dueDate, priority
+                let task = new Task(index, "inbox", titleName);
+                myToDoList.push(task);
+
+                addTaskInput.value = "";
+
+                deleteTasksInDiv();
+                showTasks();
+
+                // zmáčne za mě klávesu "cancel"
+                buttonCancelAddTask.click();
+            }
+        });
+
+
+    })
 }
+
+function showTasks() {
+    console.log(myToDoList);
+
+    const divForAllTasks = document.createElement("div");
+    divForAllTasks.setAttribute("class", "tasks-wrapper")
+    pageContent.appendChild(divForAllTasks);
+
+    for (let i = 0; i < myToDoList.length; i++) {
+
+        const divForTask = document.createElement("div");
+        divForTask.setAttribute("class", "task-wrapper")
+        divForAllTasks.appendChild(divForTask);
+
+        const divForLeftSideOfTask = document.createElement("div");
+        divForLeftSideOfTask.setAttribute("class", "left-task-wrapper")
+        divForTask.appendChild(divForLeftSideOfTask);
+
+        const divForRightSideOfTask = document.createElement("div");
+        divForRightSideOfTask.setAttribute("class", "right-task-wrapper")
+        divForTask.appendChild(divForRightSideOfTask);
+
+        const iconTask = document.createElement("i");
+        iconTask.setAttribute("class", "fa-regular fa-circle fa-xl");
+        divForLeftSideOfTask.appendChild(iconTask);
+
+        const titleOfTask = document.createElement("div");
+        titleOfTask.innerHTML = myToDoList[i].title;
+        divForLeftSideOfTask.appendChild(titleOfTask);
+
+
+        // nahradit input divem s textem "no due date" > když kliknu, nahradí mi div inputem, jakmile dám datum, tak mi vrátí div
+        // s innerhtml hodnotou datumu
+        const dateOfTask = document.createElement("input");
+        dateOfTask.setAttribute("type", "date");
+        dateOfTask.setAttribute("class", "task-date-input");
+        divForRightSideOfTask.appendChild(dateOfTask);
+
+        // prochází array
+
+        console.log(myToDoList[i]);
+    }
+}
+
+function deleteTasksInDiv() {
+    const divForAllTasks = document.querySelectorAll('.tasks-wrapper');
+
+    divForAllTasks.forEach(task => {
+        task.remove();
+    });
+}
+
